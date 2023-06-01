4d69544012807251e358d1d11f00989cc5450c30
Saumitra Lohokare <saumitra.lohokare@gmail.com>
2023-05-31 20:16:15 -0400

    Added cli arguments and additional output formats

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,25 @@ use std::{env::Args, fs, io::stdin};
 use rake::*;
 
 fn main() {
-    let mut job_descriptions_file = String::new();
+    let mut args = std::env::args();
 
-    match stdin().read_line(&mut job_descriptions_file) {
-        Ok(_) => {}
-        Err(err) => eprintln!("{err}"),
-    };
+    args.next().expect("Failed to get the program name..."); // Program name
+
+    let job_descriptions_file = args
+        .next()
+        .expect("Please provide the job descriptions file name as the first argument.");
+    let output_format = args
+        .next()
+        .expect("Please provide the output format as the second argument.");
+
+    match output_format.as_str() {
+        "md" => {}
+        "csv" => {}
+        _ => {
+            eprintln!("Output format can only be one of `md` or `csv`");
+            return;
+        }
+    }
 
     let file_contents = fs::read_to_string(job_descriptions_file.trim())
         .expect("Failed to read contents of the provided file");
@@ -19,19 +32,37 @@ fn main() {
 
     let r = Rake::new(stop_words);
 
-    println!("# Keywords found: ");
+    if &output_format == "md" {
+        println!("# Keywords found: ");
+    } else {
+        println!("Keyword,Score\n,");
+    }
 
     let mut i = 1;
     for description in descriptions {
         let keywords = r.run(description);
-        println!("## Job Description ##{i}\n");
+
+        if &output_format == "md" {
+            println!("## Job Description #{i}\n,");
+            println!("| Keyword | Score |");
+            println!("| ----- | --- |");
+        } else {
+            println!("Job Description #{i},\n");
+        }
         i += 1;
-        println!("| Keyword | Score |");
-        println!("| ----- | --- |");
 
         for KeywordScore { keyword, score } in keywords {
-            println!("| {keyword} | {score} |");
+            if &output_format == "md" {
+                println!("| {keyword} | {score} |");
+            } else {
+                println!("{keyword},{score}");
+            }
         }
-        println!("</hr>");
+
+        if &output_format == "md" {
+            println!("\n<hr>\n");
+        } else {
+            println!(",");
+        }
     }
 }
